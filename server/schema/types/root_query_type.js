98385231693a98eb05fe7073032591284b399f22
Ilya Sevrugin <isevrugin@shutterstock.com>
2019-11-01 12:12:49 -0700

    progress

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -3,11 +3,17 @@ const {
   GraphQLObjectType,
   GraphQLID
 } = graphql;
+const UserType = require('./user_type');
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    dummyField: { type: GraphQLID },
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      },
+    }
   }
 });
 
