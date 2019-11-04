a9345d2c80c1da2bec989361fa3b83af08cf62e0
Ilya Sevrugin <isevrugin@shutterstock.com>
2019-11-04 15:42:14 -0800

    progress

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Header from './Header';
+import { graphql } from 'react-apollo';
+import currentUser from '../queries/currentUser';
 
-const Dashboard = () => {
+const Dashboard = ({
+  data: { loading, user },
+}) => {
+  if (loading) return null;
+  if (!user) {
+    return (<div>You are not authenticated to see this route</div>);
+  }
+
   return (
-    <div className="container">
-      Dashboard
+    <div>
+      <h3>Dashboard</h3>
     </div>
   );
 }
 
-export default Dashboard;
+export default graphql(currentUser)(Dashboard);
