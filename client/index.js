74cfc1e28e4e63a84105af68ba31b1134d1662f1
Ilya Sevrugin <isevrugin@shutterstock.com>
2019-11-05 09:27:45 -0800

    process

--- client/index.js
+++ client/index.js
@@ -6,7 +6,7 @@ import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import LoginForm from './components/LoginForm';
-import SignupForm from './components/SignUpForm';
+import SignupForm from './components/SignupForm';
 import Dashboard from './components/Dashboard';
 import requireAuth from './components/requireAuth';
 
